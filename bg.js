daac3c8c1f7fcb1b16f6481f18a6600c517e0bfc
SB J <>
2020-03-10 23:45:09 +0900

    background function

---
+++ bg.js
@@ -0,0 +1,22 @@
+const body = document.querySelector("body");
+
+const IMG_NUMBER = 3;
+
+function paintImage(imgNumber){
+    const image = new Image();
+    image.src = `/img/${imgNumber + 1}.jpeg`;
+    image.classList.add("bgImage");
+    body.prepend(image);
+}
+
+function getRandom(){
+    const number = Math.floor(Math.random() * IMG_NUMBER);
+    return number;
+}
+
+function init(){
+    const randomNumber = getRandom();
+    paintImage(randomNumber);
+}
+
+init();
