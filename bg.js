5911967e3c9b2a4499c76bf079c64c95351ee2ba
SB J <>
2020-03-11 09:42:37 +0900

    background function completed

--- bg.js
+++ bg.js
@@ -1,10 +1,10 @@
 const body = document.querySelector("body");
 
-const IMG_NUMBER = 3;
+const IMG_NUMBER = 8;
 
 function paintImage(imgNumber){
     const image = new Image();
-    image.src = `/img/${imgNumber + 1}.jpeg`;
+    image.src = `images/${imgNumber + 1}.jpg`;
     image.classList.add("bgImage");
     body.prepend(image);
 }
